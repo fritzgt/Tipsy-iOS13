25625d2f662e9373113808a053a27d95c4719099
Fritz <fritz@icloud.com>
2022-05-13 10:56:32 -0500

    complete calculate VC

--- Tipsy/View Controllers/ViewController.swift
+++ Tipsy/View Controllers/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: - Properties
+    var tipPct = 10
     
     //MARK: - Outlets
     @IBOutlet weak var billTextField: UITextField!
@@ -18,14 +19,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
-    
 
     //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
     
     //MARK: - Actions
     @IBAction func tipChanged(_ sender: UIButton) {
@@ -37,23 +36,36 @@ class ViewController: UIViewController {
         switch selectedTip {
         case "0%":
             zeroPctButton.isSelected = true
+            tipPct = 0
         case "10%":
             tenPctButton.isSelected = true
+            tipPct = 10
         case "20%":
             twentyPctButton.isSelected = true
+            tipPct = 20
         default:
             break
         }
+        billTextField.endEditing(true)
     }
     
     @IBAction func stepperValuChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = "\(Int(sender.value))"
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        
+        if billTextField.text != "" {
+            performSegue(withIdentifier: "DetailSegue", sender: self)
+        }
     }
-    
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailSegue" {
+            guard let detailVC = segue.destination as? DetailViewController else { return }
+            detailVC.tipPct = Double(tipPct)
+            detailVC.totalAmount = Double(billTextField.text ?? "0.0")
+            detailVC.splitNum = Double(splitNumberLabel.text ?? "0.0")
+        }
+    }
 }
 
